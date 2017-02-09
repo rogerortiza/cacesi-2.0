0dcdcb79252a57a0acd90ac11d615d60efbcff93
roger <rogerortiz4@gmail.com>
2017-02-09 16:19:14 -0600

    fix urls returning from api

--- cacesi/inventario_terceros/serializers.py
+++ cacesi/inventario_terceros/serializers.py
@@ -4,7 +4,11 @@ from .models import Extintores
 class ExtintoresTercerosSerializer(serializers.ModelSerializer):
 	area = serializers.StringRelatedField()
 	cliente = serializers.StringRelatedField()
-	
+	foto = serializers.SerializerMethodField('get_foto_url')
+
+	def get_foto_url(self, obj):
+		return '%s' % (obj.foto.url)
+
 	class Meta:
 		model = Extintores
 		fields = ("__all__")
