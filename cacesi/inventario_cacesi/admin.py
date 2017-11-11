49e9c89b7735ebbc9c2bf3679aefeee82e8059f7
Rogelio Ortiz Acevedo <rogerortiza@MacBook-Pro-de-Rogelio.local>
2017-11-10 18:14:27 -0600

    new inventarios cecesi module

--- cacesi/inventario_cacesi/admin.py
+++ cacesi/inventario_cacesi/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from .models import Productos
 
 # Register your models here.
+class ProductosResource(resources.ModelResource):
+    class Meta:
+        model = Productos
+        skip_unchanged = True
+        report_skipped = False
+
+@admin.register(Productos)
+class ProductosAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'modelo', 'nombre', 'proveedor')
+    resource_class = ProductosResource
--- cacesi/inventario_cacesi/models.py
+++ cacesi/inventario_cacesi/models.py
@@ -1,3 +1,22 @@
+import datetime
 from django.db import models
+from carteras.models import Proveedores
 
 # Create your models here.
+class Productos(models.Model):
+    class Meta:
+        verbose_name_plural = "Productos"
+
+    id = models.AutoField(primary_key = True)
+    clave = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    unidad_medida = models.CharField(blank=True, null=True, max_length=100, verbose_name="Unidad de Medida")
+    proveedor = models.ForeignKey(Proveedores)
+    precio_proveedor = models.DecimalField(blank=True, null=True, max_digits=7, decimal_places=2)
+    precio_publico = models.DecimalField(blank=True, null=True, max_digits=7, decimal_places=2)
+    stock_maximo = models.IntegerField(blank=True, null=True)
+    stock_minimo = models.IntegerField(blank=True, null=True)
+    existencia = models.IntegerField(blank=True, null=True)
+    exposicion = models.IntegerField(blank=True, null=True)
+    fecha_alta = models.DateField(default=datetime.datetime.today)
