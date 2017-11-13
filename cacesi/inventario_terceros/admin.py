c9ca8e08ff397f2bbc7bb10062547278d62dddba
Rogelio Ortiz Acevedo <rogelio.ortiz@SofttekClients.com>
2017-11-13 11:46:57 -0600

    removida la foto del extintor en la vista de la lista de extintores

--- cacesi/inventario_terceros/admin.py
+++ cacesi/inventario_terceros/admin.py
@@ -4,16 +4,17 @@ from .models import Extintores
 
 # Register your models here.
 class ExtintoresAdmin(admin.ModelAdmin):
-	list_display = ('no_control', 'cliente', 'ultima_reca', 'vencimiento', 'foto_extintor')
+	list_display = ('no_control', 'cliente', 'ultima_reca', 'vencimiento')
 	exclude = ('vencimiento',)
 
 	"""def formfield_for_foreignkey(self, db_field, request, **kwargs):
 		if db_field.name == "area":
 			kwargs["queryset"] = Areas.objects.filter(area=request.user.empledos.asignaciones.area)
-			return super(ExtintoresAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)"""
+			return super(ExtintoresAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 	def foto_extintor(self, obj):
 		url = obj.foto.url
 		return format_html("<img class='img-circle' src='/"+url+"' width=100>")
+	"""
 
 admin.site.register(Extintores, ExtintoresAdmin)
