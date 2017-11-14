5675fd52c366612c6bd810d6f52fc130def784c8
Rogelio Ortiz Acevedo <rogerortiza@MacBook-Pro-de-Rogelio.local>
2017-11-13 18:31:38 -0600

    cambios: "areas unicas"

--- cacesi/catalogos/models.py
+++ cacesi/catalogos/models.py
@@ -7,7 +7,7 @@ class Areas(models.Model):
         verbose_name_plural="Areas"
 
     id = models.AutoField(primary_key = True)
-    nombre = models.CharField(max_length = 140)
+    nombre = models.CharField(max_length = 140, unique=True)
 
     def __str__(self):
         return self.nombre
